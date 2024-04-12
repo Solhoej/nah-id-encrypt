d70dccddc933e4b631664b931e07a03af0b38001
Solhoej <142444308+Solhoej@users.noreply.github.com>
2024-04-12 13:37:20 +0200

    gr

--- sketch.js
+++ sketch.js
@@ -6,11 +6,14 @@ function setup()
 
   inputBox = createInput('')
   inputBox.attribute('placeholder', 'type message in here')
+  inputBox.style('font-size', '24px');
+  inputBox.style('text-align', 'left')
   inputBox.size(400, 50);
   inputBox.position(10, 10);
 
   inputButton = createButton('Convert message');
   inputButton.size(200, 50);
+  inputButton.style('font-size', '20px')
   inputButton.position(10, 80);
 
   Machine = new ImageProcessing("Mirsad 1.jpg", "Mirsad 2.jpg")
