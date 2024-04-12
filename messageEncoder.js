567b3e3e2a81012f0b60d7df02eb08cea1313dd3
Solhoej <142444308+Solhoej@users.noreply.github.com>
2024-04-12 14:29:08 +0200

    besked position goofer op
    billedet af beskeden placere sig ikke ordentlig, og bevæger sig ud i siden. Billedet skifter heller ikke tekstens farve

--- messageEncoder.js
+++ messageEncoder.js
@@ -2,9 +2,9 @@ function messageEncoder() {
     let message = inputBox.value();
 
     if (message.trim() !== '') {
-        let graphics = createGraphics(textWidth(message), 50);
-        graphics.textSize(10);
-        graphics.textAlign(CENTER, CENTER);
+        let graphics = createGraphics(textWidth(message) * 20, 50);
+        graphics.textSize(50);
+        graphics.textAlign(LEFT, CENTER);
         graphics.fill(0);
 
         graphics.text(message, graphics.width / 2, graphics.height / 2);
@@ -14,8 +14,28 @@ function messageEncoder() {
         console.log("Image Width:", imgMessage.width);
         console.log("Image Height:", imgMessage.height);
 
+        imgMessage.loadPixels();
+
+        for (let i = 0; i < imgMessage.pixels.length; i += 4) {
+            rValue = imgMessage.pixels[i];
+            gValue = imgMessage.pixels[i + 1];
+            bValue = imgMessage.pixels[i + 2];
+
+            if (
+                rValue != 255 &&
+                gValue != 255 &&
+                bValue != 255
+            ) {
+                imgMessage.pixels[i] = 1;
+                imgMessage.pixels[i + 1] = 1;
+                imgMessage.pixels[i + 2] = 1;
+            }
+        }
+
+        imgMessage.updatePixels();
+
         image(imgMessage, 10, 500);
     } else {
-        console.log("Input box is empty");
+        console.log("Skriv noget i tekstfeltet");
     }
 }
