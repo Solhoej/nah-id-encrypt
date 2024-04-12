d9859b1db51e9b575696f7c79e936263db567ad7
Solhoej <142444308+Solhoej@users.noreply.github.com>
2024-04-12 13:29:12 +0200

    gyat

--- sketch.js
+++ sketch.js
@@ -3,6 +3,16 @@ let Machine;
 function setup() 
 {
   createCanvas(800, 800);
+
+  inputBox = createInput('')
+  inputBox.attribute('placeholder', 'type message in here')
+  inputBox.size(400, 50);
+  inputBox.position(10, 10);
+
+  inputButton = createButton('Convert message');
+  inputButton.size(200, 50);
+  inputButton.position(10, 80);
+
   Machine = new ImageProcessing("Mirsad 1.jpg", "Mirsad 2.jpg")
 }
 
